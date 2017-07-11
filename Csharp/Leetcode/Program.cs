3e756d976fd4addaf7225e0491358c90604b8fcb
mtmmy <mtmmy@users.noreply.github.com>
2017-07-11 23:25:26 +0800

    Add #2 Add Two Numbers
    The solution for problem #2, Add Two Numbers

--- Csharp/Leetcode/Program.cs
+++ Csharp/Leetcode/Program.cs
@@ -6,10 +6,28 @@ namespace Leetcode
     {
         public static void Main(string[] args)
         {
+            /* test case for #1
 			int[] testAry = { 2, 7, 11, 15 };
 			int target = 9;
 			int[] result = TwoSum.Solution(testAry, target);
 			Console.WriteLine("[" + string.Join(",", result) + "]");
+            */
+
+            /* test case for #2
+            ListNode number1 = new ListNode(2);
+            number1.next = new ListNode(4);
+            number1.next.next = new ListNode(3);
+
+            ListNode number2 = new ListNode(5);
+            number2.next = new ListNode(6);
+            number2.next.next = new ListNode(4);
+
+            ListNode result = AddTwoNumbers.Solution(number1, number2);
+
+            Console.WriteLine(result.val + " -> " + result.next.val + " -> " + result.next.next.val);
+            */
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Csharp/Leetcode/Solutions/AddTwoNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ListNode
+     {
+         public int val;
+         public ListNode next;
+         public ListNode(int x) { val = x; }
+     }
+
+    class AddTwoNumbers
+    {
+        public static ListNode Solution(ListNode l1, ListNode l2)
+        {
+            var result = new ListNode(-1);
+            var count = result;
+            double carry = 0;
+
+            while (l1 != null || l2 != null || carry > 0)
+            {
+
+                int value = (l1 != null && l1.val > 0 ? l1.val : 0) + (l2 != null && l2.val > 0 ? l2.val : 0) + (int)carry;
+                carry = value / 10;
+                carry = Math.Floor(carry);
+                value = value % 10;
+
+                count.next = new ListNode(value);
+                count = count.next;
+
+                l1 = l1 == null ? null : l1.next;
+                l2 = l2 == null ? null : l2.next;
+            }
+            return result.next;
+        }
+    }
+}
