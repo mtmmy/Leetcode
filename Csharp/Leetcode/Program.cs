e5680abf40361826226eaa733e3d5bc8fb381018
mtmmy <mtmmy@users.noreply.github.com>
2017-07-11 18:52:15 +0800

    Leetcode Problem #1

---
+++ Csharp/Leetcode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Leetcode
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int[] testAry = { 2, 7, 11, 15 };
+			int target = 9;
+			int[] result = TwoSum.Solution(testAry, target);
+			Console.WriteLine("[" + string.Join(",", result) + "]");
+        }
+    }
+}
---
+++ Csharp/Leetcode/Solutions/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    public class TwoSum
+    {
+        public TwoSum()
+        {
+        }
+
+		public static int[] Solution(int[] nums, int target)
+		{
+
+			var map = new Dictionary<int, int>();
+
+			for (var i = 0; i < nums.Length; i++)
+			{
+				int complement = target - nums[i];
+				if (map.ContainsKey(complement))
+				{
+					return new int[] { map[complement], i };
+				}
+				map[nums[i]] = i;
+			}
+			return null;
+		}
+    }
+}
