d708e88f942e9dd15c2c838a3b64e55d820565ee
mtmmy <mtmmy@users.noreply.github.com>
2017-07-22 20:55:32 +0800

    Merged K Sorted List
    First runnable version

--- Csharp/Leetcode/Program.cs
+++ Csharp/Leetcode/Program.cs
@@ -6,7 +6,24 @@ namespace Leetcode {
     class MainClass {
         public static void Main(string[] args) {
 
-            Console.WriteLine("Leetcode problems!");
+            MergeKSortedLists merge = new MergeKSortedLists();
+            
+            int size = 10;
+            ListNode[] array = new ListNode[size];
+            for (int i=0; i<size; i++) {
+                var node = new ListNode(2 * i);
+                node.next = new ListNode(2 * i + 1);
+                array[i] = node;
+            }
+
+            var result = merge.MergeKLists(array);
+            var cur = result;
+            while (cur != null) {
+                Console.Write(cur.val + ", ");
+                cur = cur.next;
+            }
+
+            //Console.WriteLine("Leetcode problems!");
             Console.ReadLine();
         }
     }
---
+++ Csharp/Leetcode/Solutions/MergeKSortedLists.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.Tools;
+
+namespace Leetcode.Solutions {
+    public class MergeKSortedLists {
+        public ListNode MergeKLists(ListNode[] lists) {
+
+            MergedTwoSortedList mergeTwo = new MergedTwoSortedList();
+            if (lists.Length == 0) {
+                return null;
+            }
+
+            var nodeList = new List<ListNode>();
+            for (int i=0; i<lists.Length; i++) {
+                nodeList.Add(lists[i]);
+            }
+
+            int nodeListTrueLength = nodeList.Count;
+            while (nodeListTrueLength > 1) {                
+                for (int i=0; i<nodeList.Count; i++) {
+                    //merge every two nodes
+                    if (2*i+1 < nodeList.Count) {
+                        //in order to simplify code, I call MergeTwoLists from another class
+                        nodeList[i] = mergeTwo.MergeTwoLists(nodeList[2 * i], nodeList[2 * i + 1]);                        
+                    } else if (2*i+1 == nodeList.Count) {                        
+                        nodeList[i] = nodeList[2 * i];
+                    }
+                }
+                if (nodeListTrueLength % 2 == 1) {
+                    nodeListTrueLength = (nodeListTrueLength + 1) / 2;
+                } else {
+                    nodeListTrueLength /= 2;
+                }
+                for (int i=nodeListTrueLength; i<nodeList.Count; i++) {
+                    nodeList[i] = null;
+                }
+            }
+
+            return nodeList[0];
+        }
+    }
+}
